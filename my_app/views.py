55ae8643f51a340b553fda12cf0cfe8d63680240
MuhammadJunaidWali <junaidwali348@gmail.com>
2026-05-06 20:53:27 +0500

    Refactor imports and remove unnecessary blank lines in views.py

--- my_app/views.py
+++ my_app/views.py
@@ -1,15 +1,13 @@
 from my_app.models import Post, Like, Comment, Share
 from my_app.serializers import PostSerializer
-from rest_framework.generics import CreateAPIView, ListAPIView , RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from django.db.models import Prefetch
-from rest_framework.permissions import IsAuthenticatedOrReadOnly , IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class PostListView(ListAPIView):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-
-
     def get_queryset(self):
         queryset = Post.objects.all().select_related('user').prefetch_related(
             Prefetch('post_likes', queryset=Like.objects.select_related('user'), to_attr='_prefetched_likes'),
@@ -24,16 +22,19 @@ class PostCreateView(CreateAPIView):
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
 
+
 class PostRetrieveView(RetrieveAPIView):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
     permission_classes = [IsAuthenticatedOrReadOnly]
     authentication_classes = [JWTAuthentication]
+
 class PostUpdateView(UpdateAPIView):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
+
 
 class PostDeleteView(DestroyAPIView):
     serializer_class = PostSerializer
