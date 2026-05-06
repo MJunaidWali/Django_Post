df166d3a6979494a18e24ddfc1d2940bcf61a747
MuhammadJunaidWali <junaidwali348@gmail.com>
2026-05-06 20:45:35 +0500

    Add CRUD endpoints for Post model in views and urls

--- my_app/views.py
+++ my_app/views.py
@@ -1,15 +1,14 @@
 from my_app.models import Post, Like, Comment, Share
 from my_app.serializers import PostSerializer
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView , RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from django.db.models import Prefetch
 from rest_framework.permissions import IsAuthenticatedOrReadOnly , IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class PostListView(ListAPIView):
-    model = Post
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    authentication_classes = [JWTAuthentication]
+
 
     def get_queryset(self):
         queryset = Post.objects.all().select_related('user').prefetch_related(
@@ -18,7 +17,26 @@ class PostListView(ListAPIView):
             Prefetch('post_shares', queryset=Share.objects.select_related('user'), to_attr='_prefetched_shares'),
         ).order_by('created_at')
         return queryset
+
 class PostCreateView(CreateAPIView):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
     permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+class PostRetrieveView(RetrieveAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    authentication_classes = [JWTAuthentication]
+class PostUpdateView(UpdateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+class PostDeleteView(DestroyAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
